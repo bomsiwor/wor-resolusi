8bd667e2e3743067bdcd940f87d4acf97506d7d0
bomsiwor <59246693+bomsiwor@users.noreply.github.com>
2022-08-20 08:53:38 +0700

    upload function php dari local

--- belajar1/pertemuan1/function.php
+++ belajar1/pertemuan1/function.php
@@ -1,1 +1,20 @@
+<?php 
+    //untuk menampilkan sebuah waktu dengan format tertentu
+    //echo date("H:i - d/M/Y");
 
+    //TIme
+    //Untuk menampilkan waktu dengan format UNIX time
+    //Waktu yang sudah berlalu sejak 1 Januari 1970
+    //echo time();
+
+    //Memprediksi hari
+    //Menampilkan hari saat ini ditambah x hari kedepan
+    //x hari kedepan = detik total
+    //echo date("l", time()+172800);
+
+    //strlen()
+    //strcmp()
+    //htmlspecialchar
+    //isset
+    //empty
+?>
